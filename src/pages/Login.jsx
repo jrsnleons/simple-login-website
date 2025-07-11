757250e8e64ea57f2805a7311c890ff413856be4
jrsn <jrsnleons@macbook-3.local>
2025-07-11 09:02:47 +0800

    added frontend for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-export default Login
+    const [message, setMessage] = React.useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            console.log("Login attempt with:", {
+                email,
+                password,
+            });
+
+            const response = await fetch("http://localhost:3000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            if (response.ok) {
+                setMessage("Login Successful. Redirecting to home page...");
+
+            } else {
+                console.log(response);
+                if (response.status === 500) {
+                    setMessage("Server error. Please try again later.");
+                } else if (response.status === 400) {
+                    setMessage("All fields are required.");
+                } else if (response.status === 401) {
+                    setMessage("Invalid email or password.");
+                } else {
+                    setMessage("Login failed. Please try again.");
+                }
+            }
+        } catch (error) {
+            console.error("Login rror:", error);
+            setMessage("An error occurred. Please try again.");
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Register</button>
+                {message && <p>{message}</p>}
+            </form>
+        </div>
+    );
+};
+
+export default Login;
